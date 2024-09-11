e3097fd6e2d6f7fb100d7712fb99272d0b924b6e
tropikais <desertukas@yahoo.com>
2024-09-11 15:09:14 +0300

    JsonData-commit

---
+++ data/demo.js
@@ -0,0 +1,13 @@
+const demoData = [
+    { id: 3, text: 'Isbandyk' },
+    { id: 4, text: 'tai' },
+    { id: 5, text: 'Dabar!' },
+];
+
+const demoText = 'ka tu?'
+
+function diff (a, b) {
+    return a - b;
+}
+
+export { demoData, demoText, diff };
---
+++ data/list.js
@@ -0,0 +1,11 @@
+export const data = [
+    { id: 3, text: 'Isbandyk' },
+    { id: 4, text: 'tai' },
+    { id: 5, text: 'Dabar!' },
+];
+
+export const text = 'ka tu?'
+
+export function sum (a, b) {
+    return a + b;
+}
